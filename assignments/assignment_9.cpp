5f10c981acfe64684af21e608a81b251fe29de85
mohammad-yeasin <78343164+mohammad-yeasin@users.noreply.github.com>
2023-09-25 20:57:21 +0800

    Create assignment_9.cpp

---
+++ assignments/assignment_9.cpp
@@ -0,0 +1,48 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+using namespace std;
+
+class TreeNode
+{
+    public:
+        int val;
+        TreeNode* left;
+        TreeNode* right;
+        
+        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+void printPreOrder(TreeNode* root)
+{
+    // base case
+    if(root == nullptr) return;
+    // recursive case
+    cout << root->val << " ";
+    printPreOrder(root->left);
+    printPreOrder(root->right);
+}
+
+// Time complixity O(h), Space complixity O()
+TreeNode* searchInBST(TreeNode* root, int inp)
+{
+    // base case
+    if(root == nullptr) return nullptr;
+    if(root->val == inp) return root;
+    // recursive case
+    if(root->val > inp) return searchInBST(root->left, inp);
+    return searchInBST(root->right, inp);
+}
+
+int main() {
+    TreeNode* root = new TreeNode(4);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(7);
+    root->left->left = new TreeNode(1);
+    root->left->right = new TreeNode(3);
+    
+    TreeNode* res = searchInBST(root, 5);
+    printPreOrder(res);
+
+    return 0;
+}
